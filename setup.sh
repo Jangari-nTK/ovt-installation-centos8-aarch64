c89104047d80b6d053d7470d54e9639620e9e2ee
Jangari-nTK <Jangari-nTK@users.noreply.github.com>
2020-10-11 20:18:01 +0900

    change installation directory

--- setup.sh
+++ setup.sh
@@ -5,8 +5,7 @@ sed -i -e "s/enabled=0/enabled=1/g" /etc/yum.repos.d/CentOS-PowerTools.repo
 dnf group install -y "Development Tools"
 
 dnf install -y glib2-devel.aarch64 libmspack-devel.aarch64 pam-devel.aarch64 openssl-devel.aarch64 \
-    xmlsec1-devel.aarch64 libxml2-devel.aarch64 libtirpc-devel.aarch64 rpcgen.aarch64 libtool-ltdl-devel \
-    git
+    xmlsec1-devel.aarch64 libxml2-devel.aarch64 libtirpc-devel.aarch64 rpcgen.aarch64 libtool-ltdl-devel
 
 dnf install -y libX11-devel.aarch64 libXext-devel.aarch64 libXinerama-devel.aarch64 libXi libXi-devel \
     libXrender-devel libXrandr-devel libXtst-devel gdk-pixbuf2-xlib-devel.aarch64 gtk3-devel.aarch64 \
@@ -17,16 +16,21 @@ git clone https://github.com/vmware/open-vm-tools.git
 cd open-vm-tools/open-vm-tools
 
 autoreconf -i
-./configure
+mkdir build
+cd build
+../configure --prefix=/usr
 make
 make install
 ldconfig
 
-ln -s /usr/local/bin/vmtoolsd /usr/bin/vmtoolsd
-ln -s /usr/local/bin/VGAuthService /usr/bin/VGAuthService
-
-cd ../../
+cd ../../../
 
 cat ./usr/lib/systemd/system/vmtoolsd.service > /usr/lib/systemd/system/vmtoolsd.service
 cat ./usr/lib/systemd/system/vgauthd.service > /usr/lib/systemd/system/vgauthd.service
 cat ./usr/lib/udev/99-vmware-scsi-udev.rules > /usr/lib/udev/99-vmware-scsi-udev.rules
+
+systemctl daemon-reload
+systemctl enable vgauthd.service
+systemctl enable vmtoolsd.service
+systemctl start vgauthd.service
+systemctl start vmtoolsd.service
